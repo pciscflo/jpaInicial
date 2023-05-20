9b7f154967a24ff0de53326a15626bdfec1db3cf
ogrumiche <98364535+ogrumiche@users.noreply.github.com>
2023-05-20 10:44:08 -0500

    04.00

--- src/main/java/pe/upc/primero/api/ApiAuthor.java
+++ src/main/java/pe/upc/primero/api/ApiAuthor.java
@@ -6,14 +6,15 @@ import pe.upc.primero.business.NegocioAuthor;
 import pe.upc.primero.entidades.Author;
 
 import java.util.List;
-
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
 @RestController
 @RequestMapping("/api")
 public class ApiAuthor {
     @Autowired
     private NegocioAuthor negocioAuthor;
 
-    @PostMapping("/author")
+    @PostMapping("/authors")
     public Author registrar(@RequestBody Author author){
         return negocioAuthor.insertar(author);
     }
@@ -21,5 +22,14 @@ public class ApiAuthor {
     public List<Author> listado(){ //Wrapper
         return negocioAuthor.listado();
     }
+    @PutMapping("/authors")
+    public Author actualizar(@RequestBody Author author) throws Exception {
+        return negocioAuthor.actualizar(author);
+    }
+    @GetMapping("/authors/{id}")
+    public Author listaId(@PathVariable(value = "id") Integer id){
+        return negocioAuthor.listaID(id);
+    }
+
 
 }
--- src/main/java/pe/upc/primero/business/NegocioAuthor.java
+++ src/main/java/pe/upc/primero/business/NegocioAuthor.java
@@ -2,6 +2,7 @@ package pe.upc.primero.business;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import pe.upc.primero.entidades.Author;
 import pe.upc.primero.repositorio.RepositorioAuthor;
 
@@ -19,5 +20,14 @@ public class NegocioAuthor {
 
     public List<Author> listado(){
         return repositorioAuthor.findAll();
+    }
+    public Author actualizar(Author author) throws Exception {
+        repositorioAuthor.findById(author.getId()).orElseThrow(() ->
+                new Exception("No se encontro entidad"));
+        return repositorioAuthor.save(author);
+    }
+
+    public Author listaID(Integer id){
+        return repositorioAuthor.findById(id).get();
     }
 }
